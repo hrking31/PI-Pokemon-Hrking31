25556a31f59816c5a13a7b1ee0b5c0e9800264e1
Hernando Rey <hrking31@gmail.com>
2023-06-06 01:47:37 -0500

    solucionado lo de las cors

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,8 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://192.168.1.18"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://192.168.1.18:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://192.168.1.18:3001");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
